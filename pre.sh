2c5397e7f6497599da0fac5795658fb69112a2c8
tuomO42 <tuaho@proton.me>
2024-07-27 01:40:36 +0300

    should propably go to sleep

--- pre.sh
+++ pre.sh
@@ -8,4 +8,6 @@ echo "exec gamemode_deathmatch_server.cfg" >> "${CSGO_FOLDER_PATH}/cfg/server.cf
 
 wget -qO- "https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git1297-linux.tar.gz" | tar xvzf - -C ./ 
 
+apt update $$ apt -y install python3
+
 python3 add_metamod_searchpath.py "${CSGO_FOLDER_PATH}"
