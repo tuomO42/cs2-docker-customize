209ded39fbed5cb1bef6a1e8a935f529e26904d9
tuomO42 <tuaho@proton.me>
2024-07-27 01:59:36 +0300

    hopefully this doesn't have a bad outcome

--- pre.sh
+++ pre.sh
@@ -8,8 +8,11 @@ echo "exec gamemode_deathmatch_server.cfg" >> "${CSGO_FOLDER_PATH}/cfg/server.cf
 
 wget -qO- "https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git1297-linux.tar.gz" | tar xvzf - -C ./ 
 
-echo "installing python3"
-echo "debug"
-sudo apt update && sudo apt -y install python3
 
-python3 add_metamod_searchpath.py "${CSGO_FOLDER_PATH}"
+# if base image doesn't have python ur fucked
+
+#echo "installing python3"
+#echo "debug"
+#sudo apt update && sudo apt -y install python3
+
+#python3 add_metamod_searchpath.py "${CSGO_FOLDER_PATH}"
