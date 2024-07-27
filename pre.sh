52c29ecd5f6114713e6948716b8450320040f8e8
tuomO42 <tuaho@proton.me>
2024-07-27 21:56:00 +0300

    fixing paths

--- pre.sh
+++ pre.sh
@@ -6,10 +6,11 @@ CSGO_FOLDER_PATH="${STEAMAPPDIR}/game/csgo"
 
 echo "exec gamemode_deathmatch_server.cfg" >> "${CSGO_FOLDER_PATH}/cfg/server.cfg"
 
-wget -qO- "https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git1297-linux.tar.gz" | tar xvzf - -C ./ 
+wget -qO- "https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git1297-linux.tar.gz" | tar xvzf - -C "${CSGO_FOLDER_PATH}"
 
 wget -q -O "temp.zip" "https://github.com/roflmuffin/CounterStrikeSharp/releases/download/v253/counterstrikesharp-with-runtime-build-253-linux-5644921.zip"
 unzip -o "temp.zip" -d "${STEAMAPPDIR}/game/csgo"
+
 wget -q -O "temp1.zip" "https://github.com/ssypchenko/cs2-gungame/releases/download/v1.1.2/GG2.plugin.1.1.2.zip"
 unzip -o "temp1.zip" -d "${STEAMAPPDIR}/game"
 # if base image doesn't have python ur fucked
